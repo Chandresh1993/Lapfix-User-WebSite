9b027720bea47f8ce34bedc24c92c6212ff2d9ba
Chandresh Kumar <chandresh@uweens.com>
2025-05-09 18:32:14 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,36 @@
-import logo from "../assets/footer-logo.png"
-
+import logo from "../assets/footer-logo.png";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
-    return (
-      <header className="bg-blue-800 text-white p-4">
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/"); // redirect to login page
+  };
+  return (
+    <header className="bg-blue-800 text-white p-4">
       <div className="flex flex-row items-center justify-between ">
-      <div className="w-28">
+        <div className="w-28">
           <img className="w-full h-full object-cover" src={logo} alt="logo" />
         </div>
-       <div> 
-       <button className="text-xl font-bold">Logout</button></div>
+        <div className="flex flex-row items-center gap-8 text-lg font-bold ">
+          <button className="uppercase">Heading</button>
+          <button className="uppercase">SubHeading</button>
+          <button className="uppercase">Products</button>
+        </div>
+        <div>
+          <button
+            onClick={handleLogout}
+            className="text-xl font-bold hover:underline uppercase"
+          >
+            Logout
+          </button>
+        </div>
       </div>
-      </header>
-    );
-  };
-  
-  export default Header;
-  
+    </header>
+  );
+};
+
+export default Header;
